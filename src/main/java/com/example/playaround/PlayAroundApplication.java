23ce2733997c98625eb90054d33172fd9efbda8a
Yujin Sung <rnlduadns98@naver.com>
2022-07-27 19:12:36 +0900

    master merge

---
+++ src/main/java/com/example/playaround/PlayAroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.playaround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayAroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayAroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/playaround/PlayAroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playaround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayAroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
