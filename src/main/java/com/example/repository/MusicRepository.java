4ef6eda84ce117864a822427f02645c7bbeb2d66
Yujin Sung <rnlduadns98@naver.com>
2022-07-28 19:34:18 +0900

    Spring 환경

---
+++ src/main/java/com/example/repository/MusicRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+import com.example.model.entity.Music;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MusicRepository extends JpaRepository<Music, Integer> {
+}
