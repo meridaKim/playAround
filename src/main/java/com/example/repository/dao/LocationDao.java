6d140614fb2f6f9ebfab7ceba2fef7237908c3b6
bona <ra_ari@naver.com>
2022-09-23 13:23:06 +0900

    Location - 엔티티, 서비스, 컨트롤러

---
+++ src/main/java/com/example/repository/dao/LocationDao.java
@@ -0,0 +1,26 @@
+package com.example.repository.dao;
+
+import com.example.model.entity.Location;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public class LocationDao {
+
+    public int insertCoordinate(SqlSessionTemplate sqlSession, Location l) {
+        return sqlSession.insert("locationMapper.insertCoordinate",l);
+    }
+
+    public int selectListCount(SqlSessionTemplate sqlSession, Location l) {
+        return sqlSession.selectOne("locationMapper.selectListCount",l);
+    }
+    public int updateCoordinate(SqlSessionTemplate sqlSession, Location l) {
+        return sqlSession.update("locationMapper.updateCoordinate",l);
+    }
+
+    public ArrayList<Location> selectMapList(SqlSessionTemplate sqlSession) {
+        return (ArrayList)sqlSession.selectList("locationMapper.selectMapList");
+    }
+}
+
---
+++ src/main/java/com/example/service/LocationServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.service;
+
+import com.example.model.entity.Location;
+import com.example.repository.dao.LocationDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class LocationServiceImpl implements LocationService {
+    @Autowired
+    private LocationDao lDao;
+
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+
+    @Override
+    public int insertCoordinate(Location l) {
+        return lDao.insertCoordinate(sqlSession,l);
+    }
+
+    @Override
+    public int selectListCount(Location l) {
+        return lDao.selectListCount(sqlSession, l);
+    }
+
+    @Override
+    public int updateCoordinate(Location l) {
+        return lDao.updateCoordinate(sqlSession,l);
+    }
+
+    @Override
+    public ArrayList<Location> selectMapList() {
+        return lDao.selectMapList(sqlSession);
+    }
+}
