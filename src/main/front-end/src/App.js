da87c27be6195f91fabe182021bca2d4e469775c
Yujin Sung <rnlduadns98@naver.com>
2022-09-26 21:48:23 +0900

    SpringBoot react 연결 + 플레이어 재구성

--- src/main/front-end/src/App.js
+++ src/main/front-end/src/App.js
@@ -1,10 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState, useEffect} from 'react';
+import React from "react";
+import {useState, useEffect} from 'react';
+import './Player.css';
+import Player from "./components/Player";
 
 function App() {
-  const [message, setMessage]=useState([]);
+
+    const [songs] = useState([
+        {
+            "musicId": 10,
+            "musicName": "별의 언어",
+            "genre": "발라드",
+            "singerName": "뉴이스트",
+            "countUp": 0,
+            "countDown": 0,
+            "img_src": "./musics_images/별의 언어_cover.jpg",
+            "src": "./musics/별의 언어.mp3"
+        },
+        {
+            "musicId": 11,
+            "musicName": "황혼의 새",
+            "genre": "발라드",
+            "singerName": "AOT",
+            "countUp": 0,
+            "countDown": 0,
+            "img_src": "./musics_images/황혼의 새_cover.jpg",
+            "src": "./musics/황혼의 새.mp3"
+        },
+        {
+            "musicId": 12,
+            "musicName": "후유증",
+            "genre": "발라드",
+            "singerName": "뉴이스트",
+            "countUp": 0,
+            "countDown": 0,
+            "img_src": "./musics_images/후유증_cover.jpg",
+            "src": "./musics/후유증.mp3"
+        },
+    ]);
+
+    const [currentSongIndex, setCurrentSongIndex] = useState(0);
+    const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+    const [message, setMessage]=useState([]);
+
   useEffect(()=>{
+      setNextSongIndex(()=>{
+          if (currentSongIndex + 1 > songs.length - 1) {
+              return 0;
+          } else {
+              return currentSongIndex + 1;
+          }
+      });
     fetch("/hello")
         .then((res)=>{
           return res.json();
@@ -12,27 +57,20 @@ function App() {
         .then((data)=>{
           setMessage(data);
         });
-  },[]);
+  },[currentSongIndex]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Player currentSongIndex={currentSongIndex}
+                setCurrentSongIndex={setCurrentSongIndex}
+                nextSongIndex={nextSongIndex}
+                songs={songs} />
           <ul>
               {message.map((v,idx)=><li key={`${idx}-${v}`}>{v}</li>)}
           </ul>
-      </header>
+
     </div>
+
   );
 }
 
