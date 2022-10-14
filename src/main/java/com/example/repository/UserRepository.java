2bb237d95d5883ac1eb1d8dbc30df9d162adb899
yonggi1234 <littedeer108@gmail.com>
2022-10-15 01:33:10 +0900

    login_register controller 구분 member_service 통합

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -2,13 +2,11 @@ package com.example.repository;
 
 
 import com.example.model.entity.Users;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, String> {
-//    List<Users> findAllByUserId(String userId);
+    Users findByUserId(String userId);
 }
