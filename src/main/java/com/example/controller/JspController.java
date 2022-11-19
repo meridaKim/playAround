4cf529e728e0bb9f1d1a50f46de152eaf44ea09b
meridaKim <sujin7187@naver.com>
2022-11-19 17:08:52 +0900

    [김수진]jspController import logger

--- src/main/java/com/example/controller/JspController.java
+++ src/main/java/com/example/controller/JspController.java
@@ -1,9 +1,13 @@
 package com.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Locale;
 
 @Controller
 public class JspController {
